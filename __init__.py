d95385327080ab815bba333dbdddab08cbb65c4e
Nassereddine <119059908+nassereddinebelghith@users.noreply.github.com>
2026-02-25 23:07:31 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,12 +1,1 @@
-"""astroinv - refactor-only split of original monolithic init.py.
-
-This package preserves the original public API by re-exporting the same symbols.
-"""
-
-from .constants import *  # noqa
-from .exceptions import *  # noqa
-from .models import *  # noqa
-from .cache import *  # noqa
-from .inventory import Inventory  # noqa
-
-__all__ = [name for name in globals().keys() if not name.startswith("_")]
+from .inventory import Inventory
